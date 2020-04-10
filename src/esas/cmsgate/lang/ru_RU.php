f0c8df97ebafe391a4a2693fcad4a8cf711c655a
n.mekh <nikita.mekh@gmail.com>
2020-04-10 13:23:18 +0300

    EPOSCG-1: интеграция с woocommerce

--- src/esas/cmsgate/lang/ru_RU.php
+++ src/esas/cmsgate/lang/ru_RU.php
@@ -1,8 +1,5 @@
 <?php
 
-use esas\cmsgate\hutkigrosh\ConfigFieldsHutkigrosh;
-use esas\cmsgate\hutkigrosh\view\client\ViewFields;
-use esas\cmsgate\view\admin\AdminViewFields;
 use esas\cmsgate\view\admin\AdminViewFieldsWoo;
 
 return array(
